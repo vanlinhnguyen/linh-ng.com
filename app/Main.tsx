bf7fdffb0c3285aa962468671dbce855c867d6b1
vanlinhnguyen <6484+vanlinhnguyen@users.noreply.ghe.oculus-rep.com>
2025-02-04 23:23:08 +0100

    linter

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,17 +12,18 @@ import { coreContent } from 'pliny/utils/contentlayer'
 const MAX_DISPLAY = 5
 const author = allAuthors.find((p) => p.slug === 'default') as Authors
 const mainContent = coreContent(author)
-export default function Home({ posts}) {
+
+export default function Home({ posts }) {
   return (
     <>
-        <div className="divide-y divide-gray-200 dark:divide-gray-700">
-          <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Linh NGUYEN
-            </h1>
-            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-              {siteMetadata.description}
-            </p>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Linh NGUYEN
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
         </div>
 
         <AuthorLayout content={mainContent}>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            My personal projects. Stay tune. 
+            My personal projects. Stay tune.
           </p>
         </div>
         <div className="container py-12">
