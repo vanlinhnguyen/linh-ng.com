629337796b0431d0248ad6c6999dabbc40a2fb6f
vanlinhnguyen <6484+vanlinhnguyen@users.noreply.ghe.oculus-rep.com>
2025-02-05 12:09:13 +0100

    change layout logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,13 +76,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href={`${basePath}/static/favicons/favicon-32x32.png`}
+        href={`${basePath}/static/favicons/apple-touch-icon.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href={`${basePath}/static/favicons/favicon-16x16.png`}
+        href={`${basePath}/static/favicons/apple-touch-icon.png`}
       />
       <link rel="manifest" href={`${basePath}/static/favicons/site.webmanifest`} />
       <link
